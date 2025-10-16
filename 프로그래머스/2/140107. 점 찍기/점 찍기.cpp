48c5d3c8b872018c7c03a2c7adfae17c828454a3
hgu <115722373+siru02@users.noreply.github.com>
2025-10-16 22:52:03 +0900

    [level 2] Title: 점 찍기, Time: 13.45 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.cpp
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.cpp
@@ -2,49 +2,21 @@
 #include <vector>
 #include <cmath>
 #include <iostream>
-
 using namespace std;
 
-long double distance(long long x, long long y) {
-    return sqrt(x*x + y*y);
-}
-
-// 투포인터로 접근, x=0, y=d부터 카운트해서 x에 대해 가능한 y를 찾으면 d/k만큼 추가
-
-/*long long solution(int k, int d) {
-    long long answer = 0;
-    long long x = 0, y = d;
-    while (x <= y) {
-        long double dist = distance(x, y);
-        cout << "(" << x << ", " << y << ") = " << dist << "\n";
-        if (dist > d) { // 현재 거리가 더 멀다면 y를 감소시킨다
-            y -= k;
-        }
-        else { // 현재 거리가 조건에 부합한다면 x를 증가시킨다
-            x += k;
-            answer += y / k;
-            if (x != y)
-                answer += y / k; // y축의 방향에서 카운트 하는 경우도 포함
-            else
-                answer += 
-            cout << "ans= "<< answer << "\n";
-        }
-    }
-    return answer;
-}*/
-
 long long solution(int k, int d) {
-    long long K = k, D = d;
-    long long ans = 0;
-
-    // y를 d 이하의 가장 큰 k의 배수로 맞춤
-    long long y = (D / K) * K;
-
+    long long answer = 0;
+    long long D = d, K = k;
+    
+    // x = 0, K, 2K, 3K, ... 형태로 순회
     for (long long x = 0; x <= D; x += K) {
-        // 현재 x에서 조건을 만족할 때까지 y를 내림
-        while (y >= 0 && x * x + y * y > D * D) y -= K;
-        if (y < 0) break;                // 이후 x에서도 더 이상 유효한 y가 없음
-        ans += (y / K) + 1;              // b = 0..(y/k) ⇒ +1 필수
+        // x가 주어졌을 때 가능한 최대 y 값 계산
+        long long maxYSquared = D * D - x * x;
+        long long maxY = (long long)sqrt(maxYSquared);
+        
+        // y는 0, K, 2K, ... 형태이므로 maxY 이하의 K의 배수 개수를 센다
+        answer += (maxY / K) + 1;
     }
-    return ans;
+    
+    return answer;
 }
